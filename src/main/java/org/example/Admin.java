32532d8ec975f8e2f4e032abf16c999b65bc8d4d
Amir Mehdi Imani <amirmehdiimani@icloud.com>
2025-05-05 14:27:26 +0330

    Update Admin.java

--- src/main/java/org/example/Admin.java
+++ src/main/java/org/example/Admin.java
@@ -3,7 +3,7 @@ package org.example;
 
 public class Admin {
     void acceptrequest() {
-        System.out.println("you send request for this job");
+        System.out.println("request for this job...");
         System.out.println("Admin accepted this request");
     }
 
